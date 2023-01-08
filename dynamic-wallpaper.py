55daa80149b162f9809ff8f5fc32db99b9c21435
Aqil <testuser-aqiL@protonmail.com>
2023-01-08 14:34:16 +0400

    Update dynamic-wallpaper.py

--- dynamic-wallpaper.py
+++ dynamic-wallpaper.py
@@ -1,13 +1,20 @@
 import os
 import random
+import time
 
 wallpapers_path = "/usr/share/backgrounds/selected/"
 wallpapers = os.listdir(wallpapers_path)
 
-wallpaper = random.choice(wallpapers)
-wallpaper_path = wallpapers_path + wallpaper 
 
 def change_wallpaper():
-    os.system(f'gsettings set org.cinnamon.desktop.background picture-uri "file://{wallpaper_path}"')
+    wallpaper = random.choice(wallpapers)
+    wallpaper_path = wallpapers_path + wallpaper
+    os.system(
+        f'gsettings set org.cinnamon.desktop.background picture-uri "file://{wallpaper_path}"'
+    )
+    print(">>>")
 
-change_wallpaper()
+
+while True:
+    time.sleep(60 * 10)  # Run every 10 minutes
+    change_wallpaper()
