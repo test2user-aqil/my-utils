3203a37b0eeef3285b1cdb9dfb85decf21818a49
Aqil <testuser-aqiL@protonmail.com>
2023-01-09 16:21:36 +0400

    Update dynamic-wallpaper.py
    Change wallpapers in sequence, preventing the same wallpaper from repeating itself

--- dynamic-wallpaper.py
+++ dynamic-wallpaper.py
@@ -1,20 +1,22 @@
 import os
-import random
 import time
 
 wallpapers_path = "/usr/share/backgrounds/selected/"
 wallpapers = os.listdir(wallpapers_path)
+count = len(wallpapers)
 
 
-def change_wallpaper():
-    wallpaper = random.choice(wallpapers)
-    wallpaper_path = wallpapers_path + wallpaper
+def change_wallpaper(path):
     os.system(
-        f'gsettings set org.cinnamon.desktop.background picture-uri "file://{wallpaper_path}"'
+        f'gsettings set org.cinnamon.desktop.background picture-uri "file://{path}"'
     )
-    print(">>>")
 
 
+i = 0
 while True:
     time.sleep(60 * 10)  # Run every 10 minutes
-    change_wallpaper()
+    change_wallpaper(wallpapers_path + wallpapers[i])
+
+    i += 1
+    if i == count:
+        i = 0
