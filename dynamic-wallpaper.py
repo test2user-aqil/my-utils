8c1d1af53da6f454ff134b604ec5b683919d393b
Aqil <testuser-aqiL@protonmail.com>
2023-01-10 12:55:31 +0400

    Update dynamic-wallpaper.py
    Causing the wallpapers to refresh at every turn

--- dynamic-wallpaper.py
+++ dynamic-wallpaper.py
@@ -6,6 +6,11 @@ wallpapers = os.listdir(wallpapers_path)
 count = len(wallpapers)
 
 
+def update():
+    wallpapers = os.listdir(wallpapers_path)
+    count = len(wallpapers)
+
+
 def change_wallpaper(path):
     os.system(
         f'gsettings set org.cinnamon.desktop.background picture-uri "file://{path}"'
@@ -16,6 +21,7 @@ i = 0
 while True:
     time.sleep(60 * 10)  # Run every 10 minutes
     change_wallpaper(wallpapers_path + wallpapers[i])
+    update()
 
     i += 1
     if i == count:
